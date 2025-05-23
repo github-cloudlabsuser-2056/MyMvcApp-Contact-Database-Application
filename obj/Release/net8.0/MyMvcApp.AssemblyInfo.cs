37bef68c1d8f74a5a145082210e120a8b00c3514
github-cloudlabsuser-2056 <github_cloudlabsuser_2056@cloudlabsaiuser.com>
2025-05-23 19:26:54 +0000

    Actualizo rutas de publish y artifact en workflow

--- obj/Release/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab6bb9a7615ed8989cdf06e94aa65c724b37392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0d766942d7c91389039b3be696ee848bb4cdff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
